f328d6c11e9a72c004420a7ecc19b4f5feb4f03e
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-04-25 15:52:07 +0300

    last update

--- javascript/script.js
+++ javascript/script.js
@@ -8,29 +8,36 @@ while (color !== "White" && color !== "Red" && color !== "Yellow") {
     color = prompt("Please give us your favorites color. 1. White , 2. Yellow , 3. Red");
 }
 
-if (color == "White"){
-document.writeln(`<h3 style="background:white;"> Welcome mr  ${userName} </h3>`)
-}else if(color == "Red"){
+if (color == "White") {
+    document.writeln(`<h3 style="background:white;"> Welcome mr  ${userName} </h3>`)
+    document.writeln(`<h3 style="background:white;"> Your Age  ${age} </h3>`)
+} else if (color == "Red") {
     document.writeln(`<h3 style="background:red;"> Welcome mr  ${userName} </h3>`)
+    
+    document.writeln(`<h3 style="background:red;"> Your Age  ${age} </h3>`)
 
-}else{
+} else {
     document.writeln(`<h3 style="background:yellow;"> Welcome mr  ${userName} </h3>`)
+    
+    document.writeln(`<h3 style="background:yellow;"> Your Age  ${age} </h3>`)
 
 }
 
-let car=prompt("What's your favorites car 1. sport , 2. classic");
+let car = prompt("What's your favorites car 1. sport , 2. classic");
 
-while (car !=="sport" && car !== "classic"){
-    car=prompt("What's your favorites car 1. sport , 2. classic");
+while (car !== "sport" && car !== "classic") {
+    car = prompt("What's your favorites car 1. sport , 2. classic");
 
 }
+
+
 
 var image;
 
-if (car == "classic"){
+if (car == "classic") {
     image = '<img src="https://thumbor.granitemedia.com/1968-ford-mustang-gt-from-bullitt/8MruAOtGQ8KLxj0L4uAsjCdlsCk=/480x352/filters:format(webp):quality(80)/granite-web-prod/85/52/855255035d5a4cbf9ea921ca327d85c6.jpeg" width="150px" height="150px" alt="Top_Greatest_CARS">'
 
-}else{
+} else {
     image = '<img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-chevrolet-corvette-z06-1607016574.jpg?crop=0.737xw:0.738xh;0.181xw,0.218xh&resize=640:*" width="150px" height="150px" alt="Top_Greatest_CARS">'
 
 }
@@ -38,7 +45,9 @@ if (car == "classic"){
 document.write(image);
 
 
-console.log("userName = " , userName);
-console.log("userAge = " , age);
-console.log("user's best color = " , color);
-console.log("car type = " , car);
+
+
+console.log("userName = ", userName);
+console.log("userAge = ", age);
+console.log("user's best color = ", color);
+console.log("car type = ", car);
